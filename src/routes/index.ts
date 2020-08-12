b4b94126e305bcf8dc072586d280bfaffb89dfe5
Marcos de Alencar Carvalho <marcoscarvalho1997@gmail.com>
2020-08-12 13:34:13 -0300

    add endpoint product

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import usersRouter from './users.routes';
+import productsRouter from './products.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/products', productsRouter);
 
 export default routes;
