05ae256483c0eb16fc85826324ff164ef9dc2404
Marcos de Alencar Carvalho <marcoscarvalho1997@gmail.com>
2020-08-12 13:32:49 -0300

    add endpoint product

---
+++ src/routes/products.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+const productsRouter = Router();
+
+/**
+ * Repositories
+ * Services
+ */
+
+productsRouter.post('/', async (request, response) => {
+  try {
+    return response.json({ message: 'Hello' });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+
+export default productsRouter;
