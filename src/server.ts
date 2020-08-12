f5d50e59f8ff29eee64fcf04e70a07756766a006
Marcos de Alencar Carvalho <marcoscarvalho1997@gmail.com>
2020-08-12 13:27:37 -0300

    add port

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.port || 3333, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server Started on port 3333!');
 });
