a0cf2f9772c3322995c5c651abe5fd5f020e2bcf
Marcos de Alencar <marco.alencar@osf.digital>
2020-09-21 16:49:13 -0300

    Configurações para o serviço de imagens

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,10 +1,13 @@
-import { Router } from 'express';
+import { request, response, Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import ProductRequestDTO from '../DTO/ProductRequestDTO';
 import ProductRepository from '../repositories/ProductRepository';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 const productsRouter = Router();
 
+const upload = multer(uploadConfig);
 
 
 productsRouter.post('/', async (request, response) => {
@@ -19,5 +22,9 @@ productsRouter.get('/:id', async (request, response) => {
   const productRepository = getCustomRepository(ProductRepository);
   return response.json({"message": id});
 });
+
+productsRouter.patch('/picture', upload.single('product_img'), async (request, response) =>{
+  return response.json({ok: true})
+})
 
 export default productsRouter;
